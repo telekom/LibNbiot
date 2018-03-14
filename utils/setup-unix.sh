d467dc53b30cd421f03f45b545a0715c68c1361c
Thorsten Krautscheid <t.krautscheid@tarent.de>
2018-03-14 18:37:17 +0100

    added build helpers for windows

---
+++ utils/setup-unix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp Makefile.unix.toplevel ../Makefile
+cp Makefile.unix.nbiot ../libNbiot/Makefile
+cp targets.mk.unix.nbiot ../libNbiot/targets.mk
+cp Makefile.unix.nbiotcore ../libNbiotCore/Makefile
+cp targets.mk.unix.nbiotcore ../libNbiotCore/targets.mk
+cp Makefile.unix.external ../external/Makefile
+cp targets.mk.unix.external ../external/targets.mk
