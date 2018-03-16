1f77647c4c626d6cd7879645807b00555e597ac3
Edgar Hindemith <edgar.hindemith@e4k.de>
2018-03-16 17:43:32 +0100

    shell script to create libnbiotApp.a from libmqttsnPaho.a, libnbiot.a and libstm.a

---
+++ createAppLib.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+PLATFORM=default
+
+cd lib/$PLATFORM
+
+ar -x libmqttsnPaho.a
+ar -x libnbiot.a
+ar -x libstm.a
+
+ar -r libnbiotApp.a *.o
+ar -r libnbiotApp.a *.co
+
+rm -f *.o
+rm -f *.co
+
+## optional: ##
+rm -f libmqttsnPaho.a
+rm -f libnbiot.a
+rm -f libstm.a
+
+cd ../..
