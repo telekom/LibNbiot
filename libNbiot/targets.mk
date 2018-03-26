179d9ae014153d509438bcc59f6c168802ed96e8
Kolja <kolja.vornholt@t-systems.com>
2018-03-26 15:04:01 +0200

    added unified file header

--- libNbiot/targets.mk
+++ libNbiot/targets.mk
@@ -1,3 +1,21 @@
+#############################################################################
+# LibNbiot: targets.mk
+#
+# Copyright (c) 2018, Edgar Hindemith, Yassine Amraue, Thorsten
+# Krautscheid, Kolja Vornholt, T-Systems International GmbH
+# contact: libnbiot@t-systems.com, opensource@telekom.de
+#
+# This file is distributed under the conditions of the Apache License,
+# Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# For details see the file LICENSE at the toplevel.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#############################################################################
+
 # Don't change anything in this file, unless you know what you are doing.
 .SUFFIXES:
 
--- utils/setup-windows.bat
+++ utils/setup-windows.bat
@@ -1,3 +1,23 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: LibNbiot: Makefile for building external libraries
+::
+:: Copyright (c) 2018, Edgar Hindemith, Yassine Amraue, Thorsten
+:: Krautscheid, Kolja Vornholt, T-Systems International GmbH
+:: contact: libnbiot@t-systems.com, opensource@telekom.de
+::
+:: This file is distributed under the conditions of the Apache License,
+:: Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: For details see the file LICENSE at the toplevel.
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
 :: Simple shell script to copy windows compatible makefiles to
 :: their destination folders. The copied files have been tested
 :: with MinGW.
