97b5b04710444c658b60818ebe7c781e8fa80d7c
Edgar Hindemith <edgar.hindemith@t-systems.com>
2018-04-03 18:26:42 +0200

    add of fileheaders to statemachine related files

--- external/src/StateMachine/actionptr.h
+++ external/src/StateMachine/actionptr.h
@@ -1,3 +1,26 @@
+/******************************************************************************
+ * File: actionptr.h
+ * Author: Edgar Hindemith
+ * This file is part of the 'Simple Statemachine for Embedded Systems'
+ * see https://github.com/edgar4k/StateMachine
+ *
+ * This file is based on the work in FP.h of Sam Grove
+ * see http://mbed.org/users/sam_grove/code/FP/
+ *
+ * Copyright (C) 2016,2018 Edgar Hindemith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 #ifndef ACTIONPTR_H
 #define ACTIONPTR_H
 
--- external/src/StateMachine/state.h
+++ external/src/StateMachine/state.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+ * File: state.h
+ * Author: Edgar Hindemith
+ * This file is part of the 'Simple Statemachine for Embedded Systems'
+ * see https://github.com/edgar4k/StateMachine
+ *
+ * Copyright (C) 2016,2018 Edgar Hindemith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 #ifndef STATE_H
 #define STATE_H
 
