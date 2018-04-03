884ef3b37f38cca6a08f614ceeda0ff71a5422ea
Thorsten Krautscheid <t.krautscheid@tarent.de>
2018-04-03 12:45:41 +0200

    added example

---
+++ examples/src/main.cpp
@@ -0,0 +1,246 @@
+// Project 
+#include "libnbiot.h"
+#include "libnbiotcore.h"
+#include "nbiottimer.h"
+#include "serialcom.h"
+#include "nbiotstring.h"
+
+// STD C++
+#include <thread>
+#include <iostream>
+#include <iomanip>
+#include <atomic>
+
+
+extern SerialCom *pSerCom;
+
+
+void dbgWrite(const unsigned char *data, unsigned short len) {
+    for (int i = 0; i < len; ++i) {
+        putchar(static_cast<int>(data[i]));
+    }
+}
+
+
+bool ticking = true;
+void invokeTick() {
+
+    const std::chrono::duration<long long, std::ratio<1LL, 1000LL> >
+            tickFrequencyMs = std::chrono::milliseconds(1);
+  
+    while (ticking) {
+        tick();
+        std::this_thread::sleep_for(tickFrequencyMs);
+    }
+}
+
+
+bool reverseFlag =false;
+int valueCounter = 0;
+int getSensorValue() {
+    if (valueCounter == 20) {
+        reverseFlag = true;
+    } else if (valueCounter == 0) {
+        reverseFlag = false;
+    }
+
+    return (reverseFlag) ? valueCounter-- : valueCounter++;
+}
+    
+
+int notification_id = 0;
+void notificationHandler(const Notification *n)
+{
+    // Handle your notifications here
+    notification_id++;
+
+    std::cout << std::setw(9) << "\033[0;32m[ DEBUG    ]\033[0m ";
+
+    std::cout << "----- New notification "
+	      << std::setw(3)
+              << std::setfill('0')
+	      << std::right
+	      << notification_id
+	      << " -----" << std::endl;
+
+    std::cout << std::setfill(' ');
+
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "State:"
+	      << n->state
+              << std::endl;
+    
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "Action:"
+              << n->action
+	      << std::endl;
+    
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "ReturnCode:"
+              << n->returnCode
+	      << std::endl;
+    
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "ErrorNumber:"
+              << n->errorNumber
+	      << std::endl;
+}
+
+bool messageArrived = false;
+int message_counter = 0;
+void subscriptionHandler(MessageData* msg)
+{
+    messageArrived = true;
+    message_counter++;
+
+    std::cout << std::setw(9)
+	      << "\033[0;32m[ DEBUG    ]\033[0m ";
+
+    std::cout << "----- New message "
+	      << std::setw(3)
+              << std::setfill('0')
+	      << std::right
+	      << message_counter
+	      << " -----"
+	      << std::endl;
+
+    std::cout << std::setfill(' ');
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "QoS:"
+              << msg->message->qos
+	      << std::endl;
+    
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "Id:"
+              << msg->message->id
+	      << std::endl;
+    
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "Payload:"
+              << (char *) msg->message->payload
+	      << std::endl;
+    
+    std::cout << std::setw(12)
+	      << "\033[0;32m[----------]\033[0m "
+	      << std::setw(14)
+	      << std::left
+	      << "Topic:"
+              << (char *) msg->topicName
+	      << std::endl;
+}
+
+
+void init()
+{
+    setDebugWriteFunction(dbgWrite);
+
+    pSerCom = new SerialCom("/dev/ttyUSB0");
+    pSerCom->connect();
+  
+    NbiotCoreConf core_conf;
+    core_conf.tickFrequency = 1000;
+    core_conf.readstatus_fu = readStatus;
+    core_conf.readbyte_fu = readByte;
+    core_conf.putchar_fu = writeByte;
+    core_conf.writestatus_fu = writeStatus;
+    core_conf.apn = "nb-cloud.ic.m2mportal.de";
+    core_conf.apnUser = "";
+    core_conf.apnPwd = "";
+    core_conf.operMccMnc = "26201";
+    core_conf.imsi = "111111111112345";
+    core_conf.imsiPwd = "thorsten";
+
+    nbiotCoreConfig(&core_conf);
+
+    NbiotConf conf;
+    conf.keepAliveInterval = 10000;
+    conf.autoPollInterval = 5;
+    conf.maxTopicCount = 3;
+    conf.gateway = "172.25.102.151";
+    conf.port = 1883;
+    conf.notify_fu = notificationHandler;
+    
+    nbiotConfig(&conf);
+
+    // Setup the statemachine, initialize internal varibles.
+    nbiotStart();
+}
+
+
+int main(int argc, char** argv) {
+
+    // Start timer thread.
+    std::thread* ticker =  new std::thread(invokeTick);
+
+    // Initialize library
+    init();
+
+    std::atomic<NbiotResult> rc(LC_Pending);
+    while(true)
+    {
+        if (isNbiotConnected())
+	{	  
+	    if (LC_Idle == rc)
+	    {
+	        // If not subscribed already, subscribe to command topic.
+	        if(!isNbiotSubscribedTo( "NBIoT/111111111112345/CMD/MyCmd") )
+		{
+		    nbiotSubscribe( "NBIoT/111111111112345/CMD/MyCmd", subscriptionHandler );
+	        }
+		
+	        if(messageArrived)
+	        {
+		    nbiotPublish("NBIoT/111111111112345/INF/MyCmd", "0", 1, QOS0);
+		    messageArrived = false;
+	        }
+		else
+		{
+	            const std::string s = std::to_string(getSensorValue());
+
+		    std::cout << std::setw(9)
+			      << "\033[0;32m[ DEBUG    ]\033[0m "
+			      << "Publish: "
+			      << s
+			      << std::endl;
+	  
+		    nbiotPublish("NBIoT/111111111112345/MES/1", s.c_str(), s.length(), QOS0);		  
+		}
+	    }	
+	}
+	else
+	{
+	    if (LC_Idle == rc){
+	        nbiotConnect(1); // Connect with cleanSession=1
+	    }
+	}
+	
+	rc = nbiotEventLoop(EventModeActive);
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    
+    ticking = false;
+    ticker->join();
+}
+
+
---
+++ examples/src/serialcom.cpp
@@ -0,0 +1,182 @@
+#include <errno.h>
+#include <unistd.h>
+#include <iostream>
+#include <sys/ioctl.h>
+#include <sys/termios.h>
+#include <fcntl.h>
+#include "serialcom.h"
+
+bool SerialCom::connect() {
+
+    std::cout << "connecting" << std::endl;
+  
+    if (!m_deviceName.empty()) {
+        int flags = O_RDWR | O_NDELAY;// | O_NONBLOCK;
+        m_fd = open(m_deviceName.c_str(), flags);
+        if (0 <= m_fd) {
+            m_isConnected = true;
+            setOptions();
+        } else {
+            m_fd = -1;
+            m_isConnected = false;
+        }
+    }
+    std::cout << "is connected: " << m_isConnected << std::endl;
+    return m_isConnected;
+}
+
+unsigned char SerialCom::readByte() {
+    unsigned char ret = 0;
+    if (m_isConnected) {
+        int byteRead = ::read(m_fd, &ret, 1);
+        if (1 != byteRead) {
+            ret = 0;
+        }
+    }
+    return ret;
+}
+
+ReadStatus SerialCom::readStatus() {
+    ReadStatus ret = rx_empty;
+    if (m_isConnected) {
+        ret = rx_avail;
+    }
+    return ret;
+}
+
+void SerialCom::writeByte(unsigned char byte) {
+    if (m_isConnected) {
+        ::write(m_fd, &byte, 1);
+    }
+}
+
+WriteStatus SerialCom::writeStatus() {
+    WriteStatus ret = tx_full;
+    if (m_isConnected) {
+        ret = tx_empty;
+    }
+    return ret;
+}
+
+bool SerialCom::disconnect() {
+    return !m_isConnected;
+}
+
+void SerialCom::setOptions() {
+    struct termios newtio;
+    if (tcgetattr(m_fd, &newtio) != 0) {
+        std::cerr << "tcgetattr() 3 failed" << std::endl;
+    }
+
+    cfsetospeed(&newtio, m_settings.baud);
+    cfsetispeed(&newtio, m_settings.baud);
+
+    /* We generate mark and space parity ourself. */
+    if (m_settings.databits == 7 && (m_settings.parity == "Mark" || m_settings.parity == "Space")) {
+        m_settings.databits = 8;
+    }
+    switch (m_settings.databits) {
+        case 5:
+            newtio.c_cflag = (newtio.c_cflag & ~CSIZE) | CS5;
+            break;
+        case 6:
+            newtio.c_cflag = (newtio.c_cflag & ~CSIZE) | CS6;
+            break;
+        case 7:
+            newtio.c_cflag = (newtio.c_cflag & ~CSIZE) | CS7;
+            break;
+        case 8:
+        default:
+            newtio.c_cflag = (newtio.c_cflag & ~CSIZE) | CS8;
+            break;
+    }
+    newtio.c_cflag |= CLOCAL | CREAD;
+
+    //parity
+    newtio.c_cflag &= ~(PARENB | PARODD);
+    if (m_settings.parity == "Even") {
+        newtio.c_cflag |= PARENB;
+    } else if (m_settings.parity == "Odd") {
+        newtio.c_cflag |= (PARENB | PARODD);
+    }
+
+    newtio.c_cflag &= ~CRTSCTS;
+
+    //stopbits
+    if (m_settings.stop == 2) {
+        newtio.c_cflag |= CSTOPB;
+    } else {
+        newtio.c_cflag &= ~CSTOPB;
+    }
+
+    newtio.c_iflag = IGNBRK;
+
+    //software handshake
+    if (m_settings.softwareHandshake) {
+        newtio.c_iflag |= IXON | IXOFF;
+    } else {
+        newtio.c_iflag &= ~(IXON | IXOFF | IXANY);
+    }
+
+    newtio.c_lflag = 0;
+    newtio.c_oflag = 0;
+
+    newtio.c_cc[VTIME] = 1;
+    newtio.c_cc[VMIN] = 60;
+
+    if (tcsetattr(m_fd, TCSANOW, &newtio) != 0) {
+        std::cerr << "tcsetattr() 1 failed" << std::endl;
+    }
+
+    int mcs = 0;
+    ioctl(m_fd, TIOCMGET, &mcs);
+    mcs |= TIOCM_RTS;
+    ioctl(m_fd, TIOCMSET, &mcs);
+
+    if (tcgetattr(m_fd, &newtio) != 0) {
+        std::cerr << "tcgetattr() 4 failed" << std::endl;
+    }
+
+    //hardware handshake
+    if (m_settings.hardwareHandshake) {
+        newtio.c_cflag |= CRTSCTS;
+    } else {
+        newtio.c_cflag &= ~CRTSCTS;
+    }
+    if (tcsetattr(m_fd, TCSANOW, &newtio) != 0) {
+        std::cerr << "tcsetattr() 2 failed" << std::endl;
+    }
+
+}
+
+SerialCom *pSerCom = nullptr;
+
+unsigned char readByte() {
+    unsigned char ret = 0;
+    if (nullptr != pSerCom) {
+        ret = pSerCom->readByte();
+    }
+    return ret;
+}
+
+ReadStatus readStatus() {
+    ReadStatus ret = rx_empty;
+    if (nullptr != pSerCom) {
+        ret = pSerCom->readStatus();
+    }
+    return ret;
+}
+
+void writeByte(unsigned char byte) {
+    if (nullptr != pSerCom) {
+        pSerCom->writeByte(byte);
+    }
+}
+
+WriteStatus writeStatus() {
+    WriteStatus ret = tx_full;
+    if (nullptr != pSerCom) {
+        ret = pSerCom->writeStatus();
+    }
+    return ret;
+}
