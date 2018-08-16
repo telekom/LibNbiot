92f29a1b7d4b87f192df82c36b1acf6a9fd9b5d2
Thorsten Krautscheid <t.krautscheid@gmx.de>
2018-08-16 15:34:01 +0200

    adjusted debug settings

--- libNbiotCore/include/nbiotdebug.h
+++ libNbiotCore/include/nbiotdebug.h
@@ -12,7 +12,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ========================================================================
@@ -21,36 +21,25 @@
 #ifndef NBIOTDEBUG_H
 #define NBIOTDEBUG_H
 
-#ifndef DEBUG
-#define DEBUG /*!< global debugging output on or off */
-#undef DEBUG
-#endif
+//#ifndef DEBUG
+//#define DEBUG /*!< global debugging output on or off */
+//#undef DEBUG
+//#endif
 #define DEBUGLINE_LENGTH (256u) /*!< maximum length of a single debugging output line */
 #ifdef DEBUG
 
 #define DEBUG_COLOR
-// #undef DEBUG_COLOR
 
-#define DEBUG_SERIAL /*!< debugging output for the class Serial */
-#undef DEBUG_SERIAL
-
-#define DEBUG_SERIAL_VERBOSE /*!< verbose debugging output for the method Serial::readLine */
-#undef DEBUG_SERIAL_VERBOSE
-
-#define DEBUG_SERIAL_RAW /*!< debugging output for the method Serial::readRaw */
-#undef DEBUG_SERIAL_RAW
-
-#define DEBUG_ATCOMMANDS /*!< debugging output for the class AtCommands */
-// #undef DEBUG_ATCOMMANDS
-
-#define DEBUG_MODEM /*!< debugging output for the classes NbiotModemTools and NbiotConnectivity */
-#undef DEBUG_MODEM
+// #define DEBUG_SERIAL /*!< debugging output for the class Serial */
+// #define DEBUG_SERIAL_VERBOSE /*!< verbose debugging output for the method Serial::readLine */
+// #define DEBUG_SERIAL_RAW /*!< debugging output for the method Serial::readRaw */
+// #define DEBUG_ATCOMMANDS /*!< debugging output for the class AtCommands */
+// #define DEBUG_MODEM /*!< debugging output for the classes NbiotModemTools and NbiotConnectivity */
 
 #define DEBUG_MQTT /*!< debugging output for LibNbiot classes */
-//#undef DEBUG_MQTT
 
-#define DEBUG_STRING /*!< debugging output for the nbiot::string class */
-#undef DEBUG_STRING
+// #define DEBUG_STRING /*!< debugging output for the nbiot::string class */
+
 #endif
 
 #ifdef __cplusplus
